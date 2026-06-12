0cc0870fc20b97e2c8e4ffc1106e81260964fb63
Samiksha130007 <ingolesamiksha007@gmail.com>
2026-06-12 09:27:08 +0530

    Rename user_details.py to question9.py

---
+++ question9.py
@@ -0,0 +1,4 @@
+Name=input("Enter a name:")
+Age=int(input("enter a age:"))
+
+print("Hello", Name, ", you will turn", Age + 1, "next year.")
--- user_details.py
+++
@@ -1,4 +0,0 @@
-Name=input("Enter a name:")
-Age=int(input("enter a age:"))
-
-print("Hello", Name, ", you will turn", Age + 1, "next year.")
